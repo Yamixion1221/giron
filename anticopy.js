f010711e1a4b594bdaadb41e69793197ba9c3058
Yamixion1221 <yamixion@gmail.com>
2025-07-10 15:21:06 +0700

    Create anticopy.js

---
+++ anticopy.js
@@ -0,0 +1,24 @@
+document.addEventListener("contextmenu", function(e){
+  e.preventDefault();
+alert("Akses shortcut ini diblokir!");
+});
+
+// Blok shortcut keyboard
+document.addEventListener("keydown", function(e) {
+  // Daftar kombinasi yang diblok
+  if (
+    e.key === "F12" || 
+    (e.ctrlKey && ["c", "u", "s", "a", "f", "+", "=", "-", "p"].includes(e.key.toLowerCase())) || 
+    (e.ctrlKey && e.shiftKey && ["i", "j"].includes(e.key.toLowerCase()))
+  ) {
+    e.preventDefault();
+    console.log("Shortcut diblokir");
+  }
+});
+
+// Blok zoom menggunakan Ctrl + Scroll
+window.addEventListener("wheel", function(e) {
+  if (e.ctrlKey) {
+    e.preventDefault();
+  }
+}, { passive: false });
